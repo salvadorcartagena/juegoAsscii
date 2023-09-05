14461fb5ab629e0ded3d529ee96c543eee858703
Salvador CR <salvador.cartagena@gmail.com>
2023-09-05 16:50:15 -0500

    ingresar por teclado

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 print("Ingresa tu Nombre: ")
 print("Hola" ,input(), "Bienvenido al juego")
+
+
+import readchar
+from readchar import readkey, key
+while true:
+    k = readchar.readkey()
+    if ord(key) == 000:
+        print(f"ingrese dato:")
+        break
